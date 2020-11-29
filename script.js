bed66b6777d1f8d63a7df5927cdec0e2c1d63c1f
vaso1943 <Vaso9229@gmail.com>
2020-11-29 14:48:28 +0300

    new

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-
-
-
-
 $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").tooltip();
+
+
+
+    let ans = document.createElement("div")
+
+    $(".modal-body").load("https://artart241.github.io a.nav-link.active", function (response, status, request){
+
+    });
+});
+
+
+$("#btnSelect").click(function (e) {
+    console.log($("option[selected]"))
 });
