2803ab2b81f7cdd5bede8b7df82fe2683c65031f
vaso1943 <Vaso9229@gmail.com>
2020-12-06 14:37:14 +0300

    Final

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").tooltip();
-
-
-
-    let ans = document.createElement("div")
-
-    $(".modal-body").load("https://artart241.github.io a.nav-link.active", function (response, status, request){
-
-    });
 });
 
 
 $("#btnSelect").click(function (e) {
-    console.log($("option[selected]"))
+    let select = document.querySelector("select.custom-select")
+    let url = select.options[select.selectedIndex].value + " " + "div#answer"
+
+    document.querySelector("#msgLabel").innerHTML = "Cообщение от пользователя " + select.options[select.selectedIndex].innerHTML;
+
+
+    $(".modal-body").load(url, function (response, status, request){
+        document.querySelector(".modal-body>div#answer").id="";
+    
+
+        $("#msgBox").modal("show")
+    });
 });
+
+ 
